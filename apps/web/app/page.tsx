29ceb4850201a48e37a741b06ec983ae01d11cef
Michael van Straten <mvanstraten@mozilla.com>
2025-09-10 18:30:38 +0200

    Test commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,3 @@
 export default function HomePage() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold underline">Hello mom!</h1>;
 }
